382c51d0b5cd559623223842def9945c24aaa980
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-12-30 19:01:18 +0100

    Created sidebar layout

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,7 +2,70 @@ import React, { Component } from "react";
 
 class Sidebar extends Component {
   render() {
-    return <nav className="sidebar">sidebar</nav>;
+    return (
+      <nav className="sidebar">
+        <ul className="sidebar__list">
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link active">
+              Overview
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Likes
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Playlists
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Stations
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Albums
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Followings
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              History
+            </a>
+          </li>
+        </ul>
+
+        <ul className="sidebar__list">
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              About
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Support
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Settings
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="" className="sidebar__link">
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
 
