dae9593fc5934a9a9fa9a6f943a74be03fd2790a
Karol-Waliszewski <karolwaliszewski@gmail.com>
2020-01-02 22:46:47 +0100

    Added new .row modifier for flex-start

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,41 +8,40 @@ class Home extends Component {
     return (
       <div className="route">
         <Section title="Recently played" link="/history">
-          <div className="row">
-            <Track title="lorem" author="ipsum"></Track>
+          <div className="row--start">
+            <Track title="lorem ipsum lorem ipsum" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
           </div>
         </Section>
         <Section title="Your favourite" link="/likes">
-          <div className="row">
+          <div className="row--start">
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
-            <Track title="lorem" author="ipsum"></Track>
-            <Track title="lorem" author="ipsum"></Track>{" "}
           </div>
         </Section>
         <Section title="Playlists" link="/playlists">
-          <div className="row">
+          <div className="row--start">
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
           </div>
         </Section>
         <Section title="Albums" link="/albums">
-          <div className="row">
+          <div className="row--start">
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
             <Track title="lorem" author="ipsum"></Track>
           </div>
         </Section>
         <Section title="Followings" link="/followings">
-          <div className="row">
+          <div className="row--start">
             <User name="Lorem ipsum"></User>
             <User name="Lorem ipsum"></User>
             <User name="Lorem ipsum"></User>
+            <User name="Lorem ipsum lorem ipsum"></User>
           </div>
         </Section>
       </div>
