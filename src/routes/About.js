ef1f69b97b6c7a1babcc2c87a85d0002b9aaf7a2
Karol-Waliszewski <karolwaliszewski@gmail.com>
2020-05-19 22:36:46 +0200

    Styled about page the same as home

--- src/routes/About.js
+++ src/routes/About.js
@@ -4,23 +4,35 @@ class About extends Component {
   render() {
     return (
       <div className="route about">
-        <p className="about__text">
-          SoundCloud desktop is a hobby made project created by{" "}
-          <a href="https://github.com/Karol-Waliszewski">me</a>.
-        </p>
-        <p className="about__text">
-          I was tired of my SoundCloud drowning in my browsers tab, but
-          unfortunetely there is no official desktop application. So I decided
-          to give it a try and with available API do one by myself.
-        </p>
-        <p className="about__text">
-          <a href="https://github.com/Karol-Waliszewski/SoundCloud-Desktop">
-            Project on Github.
-          </a>
-        </p>
-        <p className="about__text">
-          <a href="https://github.com/Karol-Waliszewski">My Github.</a>
-        </p>
+        <h2 className="about__heading">About</h2>
+        <div className="about__content">
+          <p>
+            SoundCloud desktop is a hobby made project created by{" "}
+            <a
+              href="https://github.com/Karol-Waliszewski"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Karol Waliszewski
+            </a>
+            .
+          </p>
+          <p>
+            The reason why this application is being made is because I was tired
+            of my SoundCloud page drowning into my browser's tabs, but
+            unfortunately there is no official desktop application. So I decided
+            to give it a try and with available API do one by myself.
+          </p>
+          <p>
+            <a
+              href="https://github.com/Karol-Waliszewski/SoundCloud-Desktop"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Project on Github.
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
